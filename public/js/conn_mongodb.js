a8af81b6aa985a6070810b361a47c14379253992
Guillermo <cobain.memo@gmail.com>
2018-03-28 11:45:09 -0600

    1

---
+++ public/js/conn_mongodb.js
@@ -0,0 +1,77 @@
+var myobj;
+
+function getElem() {
+    var mod = document.getElementById("mod").value;
+    var rap = document.getElementById("rap").value;
+    var son = document.getElementById("son").value;
+    var grap = document.getElementById("grap").value;
+    var sat = document.getElementById("sat").value;
+    var rec = document.getElementById("rec").value;
+    var comm = document.getElementById("comm").value;
+    var email = document.getElementById("email").value;
+
+    var myobj = { Modelo: '"'+mod+'"', 
+        Rapidez: '"'+rap+'"', 
+        Sonido: '"'+son+'"', 
+        Graficos: '"'+grap+'"', 
+        Satisfaccion: '"'+sat+'"', 
+        Recomm: '"'+rec+'"', 
+        Comentario: '"'+comm+'"', 
+        Correo: '"'+email+'"'};
+    //alert(JSON.stringify(obj));
+}
+
+function importarScript(nombre) {
+    var s = document.createElement("script");
+    s.src = nombre;
+    document.querySelector("head").appendChild(s);
+}
+
+function connParse() {
+    importarScript("js/parse.js");
+    var Parse = requirejs('parse');
+    Parse.initialize("steelcaseErgoSeatEvaId", "steelcaseErgoSeatEvaMasterKey");
+    Parse.serverURL = 'https://steelcase-ergo-seat-eva.herokuapp.com/parse'
+}
+
+function addElement() {
+    connParse();
+
+
+    getElem();
+
+    var query = new Parse.Query("testNieu");
+    query.aggregate(myobj)
+    .then(function(results) {
+        // results contains sum of score field and stores it in results[0].total
+    })
+    .catch(function(error) {
+        // There was an error.
+    });
+}
+
+/*function insert(){
+    
+    connParse();
+
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://heroku_r2m6vcd3:kgbto69i8j3taqpcitt405fe29@ds131826.mlab.com:31826/heroku_r2m6vcd3";
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("heroku_r2m6vcd3");
+        getElem();
+        //var myobj = { Modelo: "Legion Y520", Rapidez: 2, Sonido: 1, Graficos: 1, Satisfaccion: 2, Recomm: "si", Comentario: "lalala", Correo: "coba@gmail.com"};
+        dbo.collection("testNieu").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            //console.log("1 document inserted");
+            alert("Respuestas ingresadas");
+            db.close();
+        });
+    });
+}*/
+
+
+
+
+
