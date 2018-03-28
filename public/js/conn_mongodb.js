9f10c7d93f209f72c8751f1e2a589a4df27cae7c
Guillermo <cobain.memo@gmail.com>
2018-03-28 15:02:30 -0600

    10

--- public/js/conn_mongodb.js
+++ public/js/conn_mongodb.js
@@ -30,7 +30,7 @@ function addElement() {
         Comentario: '"'+comm+'"', 
         Correo: '"'+email+'"'};
     
-    Parse.initialize("steelcaseErgoSeatEvaId");
+    Parse.initialize("steelcaseErgoSeatEvaId", "steelcaseErgoSeatEvaKey");
     Parse.serverURL = 'https://steelcase-ergo-seat-eva.herokuapp.com/parse'
 
     var query = new Parse.Query("testNieu");
